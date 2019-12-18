00db932a69047f2fe31d633bc581efe09f1a484d
Thomas Villain <atelier@anybodesign.com>
2019-12-18 15:14:57 +0100

    New block posts

--- ad-acf-blocks-fields.php
+++ ad-acf-blocks-fields.php
@@ -43,6 +43,13 @@ acf_add_local_field_group(array(
 				'value' => 'acf/gallery',
 			),
 		),
+		array(
+			array(
+				'param' => 'block',
+				'operator' => '==',
+				'value' => 'acf/posts',
+			),
+		),
 	),
 	'menu_order' => -1,
 	'position' => 'normal',
@@ -665,6 +672,111 @@ acf_add_local_field_group(array(
 ));
 
 acf_add_local_field_group(array(
+	'key' => 'group_5dfa2713d3b09',
+	'title' => __('AD ACF Block 05: Posts', 'adblocks'),
+	'fields' => array(
+		array(
+			'key' => 'field_5dfa27874faba',
+			'label' => __('Select one or many publications', 'adblocks'),
+			'name' => 'posts_select',
+			'type' => 'relationship',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'post_type' => '',
+			'taxonomy' => '',
+			'filters' => array(
+				0 => 'search',
+				1 => 'post_type',
+			),
+			'elements' => array(
+				0 => 'featured_image',
+			),
+			'min' => '',
+			'max' => '',
+			'return_format' => 'object',
+		),
+		array(
+			'key' => 'field_5dfa2bcaa5483',
+			'label' => __('Title level', 'adblocks'),
+			'name' => 'title_level',
+			'type' => 'select',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '50',
+				'class' => '',
+				'id' => '',
+			),
+			'choices' => array(
+				'h2' => __('H2', 'adblocks'),
+				'h3' => __('H3', 'adblocks'),
+				'h4' => __('H4', 'adblocks'),
+				'h5' => __('H5', 'adblocks'),
+				'h6' => __('H6', 'adblocks'),
+			),
+			'default_value' => array(
+				0 => 'h2',
+			),
+			'allow_null' => 0,
+			'multiple' => 0,
+			'ui' => 1,
+			'ajax' => 0,
+			'return_format' => 'value',
+			'placeholder' => '',
+		),
+		array(
+			'key' => 'field_5dfa2c4cc8bb0',
+			'label' => __('Content to show', 'adblocks'),
+			'name' => 'content_show',
+			'type' => 'button_group',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '50',
+				'class' => '',
+				'id' => '',
+			),
+			'choices' => array(
+				'excerpt' => __('Excerpt', 'adblocks'),
+				'content' => __('Full content', 'adblocks'),
+			),
+			'default_value' => array(
+				0 => 'excerpt',
+			),
+			'allow_null' => 0,
+			'default_value' => '',
+			'layout' => 'horizontal',
+			'return_format' => 'value',
+		),
+	),
+	'location' => array(
+		array(
+			array(
+				'param' => 'block',
+				'operator' => '==',
+				'value' => 'acf/posts',
+			),
+		),
+	),
+	'menu_order' => 0,
+	'position' => 'normal',
+	'style' => 'default',
+	'label_placement' => 'top',
+	'instruction_placement' => 'label',
+	'hide_on_screen' => '',
+	'active' => true,
+	'description' => '',
+));
+
+acf_add_local_field_group(array(
 	'key' => 'group_5df9ebd7beef8',
 	'title' => __('Block Options: Gallery Pictures', 'adblocks'),
 	'fields' => array(
@@ -912,6 +1024,13 @@ acf_add_local_field_group(array(
 				'param' => 'block',
 				'operator' => '==',
 				'value' => 'acf/cta',
+			),
+		),
+		array(
+			array(
+				'param' => 'block',
+				'operator' => '==',
+				'value' => 'acf/posts',
 			),
 		),
 	),
--- ad-acf-blocks.php
+++ ad-acf-blocks.php
@@ -212,7 +212,22 @@ function adblocks_init() {
             'keywords'			=> array( 'cta', 'call to action' ),
             'render_template'   => ADBLOCKS__PLUGIN_PATH . 'templates/block-cta.php',
             'enqueue_style' 	=> ADBLOCKS__PLUGIN_URL . 'css/block-cta.css',
-		));		
+		));
+		
+		// Posts
+		
+		acf_register_block(array(
+			'name'				=> 'posts',
+			'title'				=> __('Posts', 'adblocks'),
+			'description'		=> __('Insert one or many posts or custom posts.', 'adblocks'),
+			'category'			=> 'ad-blocks',
+			'icon'				=> '<svg width="100%" height="100%" viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;"><g><path d="M12,0l0,2l-9.2,0c-0.442,0 -0.8,0.359 -0.8,0.8l0,18.4c0,0.442 0.358,0.8 0.8,0.8l18.4,0c0.442,0 0.8,-0.358 0.8,-0.8l0,-9.2l2,0l0,10.08c0,1.06 -0.86,1.92 -1.92,1.92l-20.16,0c-1.06,0 -1.92,-0.86 -1.92,-1.92l0,-20.16c0,-1.06 0.86,-1.92 1.92,-1.92l10.08,0Z" style="fill:#555d66;"/><path d="M18.018,9.97l-0.997,0.748c-0.997,0.997 -1.246,2.741 -0.498,3.988l-1.994,1.994l-2.742,-2.742l-3.24,3.24c-0.499,0.498 -3.988,3.24 -4.487,2.742c-0.498,-0.499 2.244,-3.988 2.742,-4.487l3.24,-3.24l-2.742,-2.742l1.994,-1.994c1.247,0.748 2.991,0.748 3.988,-0.498l0.748,-0.748c1.246,-1.246 1.246,-2.991 0.499,-4.237l1.994,-1.994l7.477,7.228l-1.994,1.994c-1.191,-0.476 -2.837,-0.497 -3.988,0.748Z" style="fill:#555d66;"/></g></svg>',
+            'mode'				=> 'auto',
+            'supports'			=> array( 'align' => array( 'wide', 'full' ), 'mode' => false),
+            'keywords'			=> array( 'post', 'publication' ),
+            'render_template'   => ADBLOCKS__PLUGIN_PATH . 'templates/block-posts.php',
+            'enqueue_style' 	=> ADBLOCKS__PLUGIN_URL . 'css/block-posts.css',
+		));			
 
 	}	
 }
