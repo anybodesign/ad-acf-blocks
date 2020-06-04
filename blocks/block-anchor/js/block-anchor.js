71618dccd97eb615bbd73598c731e449bf11dab2
Thomas Villain <atelier@anybodesign.com>
2020-06-04 17:43:31 +0200

    Cleaning

--- blocks/block-anchor/js/block-anchor.js
+++ blocks/block-anchor/js/block-anchor.js
@@ -30,29 +30,6 @@
 
 })(jQuery);
 
-/*
-
-jQuery(document).ready(function($) {
-	
-	function scrollToAnchor(anchorID){
-		var target = $(anchorID);
-		var targetOffset = target.attr('data-offset');
-		var targetSpeed = target.attr('data-speed');
-
-		$('html,body').animate({scrollTop: target.offset().top - targetOffset}, targetSpeed);
-	}
-
-	$('a[href^="#"]').not('a[href="#"]').on('click', function() {
-		var targetID = $(this).attr('href');
-		scrollToAnchor( targetID );
-		
-		return false;
-	});	
-	
-});
-*/
-
-
 
 
 // Vanilla JavaScript Scroll to Anchor
